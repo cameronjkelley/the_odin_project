1742829f0173c352aaae80b430aa7163d94ab676
Cameron Kelley <i.am.cjk@gmail.com>
2016-01-16 16:10:14 -0500

    initial commit

---
+++ calculator/calc/calculator.js
@@ -0,0 +1,72 @@
+var total = null,
+	operand = "",
+	operator;
+
+$(document).ready(function() {
+	display(0);
+});
+
+$(".number").click(function() {
+	operand += $.trim($(this).text());
+	display(operand);
+});
+
+$(".operator").click(function() {
+	operator = $.trim($(this).text());
+	if(total === null) {
+		total = operand;
+		operand = "";
+	}
+});
+
+$("#equals-btn").click(function() {
+	if(total != null) {
+		var a = parseInt(total),
+			b = parseInt(operand);
+		switch(operator) {
+			case "+":
+				total = adding(a, b);
+				break;
+			case "-":
+				total = subtracting(a, b);
+				break;
+			case "*": 
+				total = multiplying(a, b)
+				break;
+			case "/":
+				total = dividing(a, b);
+				break;
+		}
+		display(total);
+		operand = "";
+	}
+});
+
+$("#clear-btn").click(function() {
+	clear();
+});
+
+function adding(a, b) {
+	return a + b;
+};
+
+function subtracting(a, b) {
+	return a - b;
+};
+
+function multiplying(a, b) {
+	return a * b;
+};
+
+function dividing(a, b) {
+	return a / b;
+};
+
+function clear() {
+	total = null, operand = "", operator = "";
+	display(0);
+};
+
+function display(x) {
+	$("#display").html(x);
+};
---
+++ calculator/warmup.js
@@ -0,0 +1,28 @@
+var my_max = function(ary) {
+	var max = 0;
+	for(var i = 0; i < ary.length; i ++) {
+		if(ary[i] > max) {
+			max = ary[i];
+		}
+	}
+	return max;
+};
+
+var vowel_count = function(str) {
+	var vowels = ["a", "e", "i", "o", "u", "y"]
+		count = 0;
+		for(var i = 0; i < str.length; i++) {
+			if(vowels.indexOf(str[i].toLowerCase()) !== -1) {
+				count += 1;
+			}
+		}
+	return count;
+};
+
+var reverse = function(str) {
+	var reverse = "";
+	for(var i = 0; i < str.length; i++) {
+		reverse += str[i];
+	}
+	return reverse;
+};
