a9cf68aa43ab9faf15fc2fbcdd1aba4cb0c904a7
Cameron Kelley <i.am.cjk@gmail.com>
2016-01-21 16:04:50 -0500

    reorganized repo

---
+++ javascript/restaurant/README.md
@@ -0,0 +1,3 @@
+##[Project: Manipulating the DOM with jQuery](http://www.theodinproject.com/javascript-and-jquery/manipulating-the-dom-with-jquery?ref=lnav)
+
+View it [here]()
---
+++ javascript/restaurant/scripts/restaurant.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+	$("#home").css("display", "block");
+});
+
+$(".nav-link").click(function() {
+	$(".content-div").css("display", "none");
+	var id = $(this).attr("id");
+	if(id === "home-link") {
+		$("#home").css("display", "block");
+	} else if(id === "menu-link") {
+		$("#menu").css("display", "block");
+	} else if(id === "reviews-link") {
+		$("#reviews").css("display", "block");
+	} else {
+		$("#contact").css("display", "block");
+	}
+});
+
+var reviewImages = [
+	"images/01_6.jpeg",
+	"images/02_6.jpeg", 
+	"images/03_6.jpeg", 
+	"images/04_6.jpeg", 
+	"images/05_6.jpeg", 
+	"images/06_5.jpeg", 
+	"images/07_5.jpeg", 
+	"images/08_4.jpeg"
+];
+
+$(".arrow").click(function() {
+	if($(this).attr("id") === "prev-arrow") {
+		changeSlide(-1);
+	} else {
+		changeSlide(1);
+	}
+});
+
+$(document).keyup(function(event) {
+	if(event.which == 37) {
+		changeSlide(-1);
+	} else if(event.which == 39) {
+		changeSlide(1);	
+	}
+});
+
+function changeSlide(value) {
+	imgSrc = $("#review-image").attr("src");
+	imgIndex = reviewImages.indexOf(imgSrc);
+	$("#review-image").attr("src", reviewImages[imgIndex + value]);
+};
