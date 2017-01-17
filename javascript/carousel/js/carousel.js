0c4e67bb5d82c2c44d110ce71b8308c120f4ea59
Cameron Kelley <i.am.cjk@gmail.com>
2017-01-17 15:59:56 -0500

    Update carousel.js

--- javascript/carousel/js/carousel.js
+++ javascript/carousel/js/carousel.js
@@ -1,36 +1,19 @@
-var $slides 	 = $('.slide'),
-	$firstSlide  = $slides[0],
-	$lastSlide   = $slides[$slides.length - 1],
-	$dots 		 = $('.dot'),
-	$firstDot 	 = $dots[0],
-	$lastDot 	 = $dots[$dots.length - 1]
-	timer        = function() { setInterval(nextSlide, 5000); };			
-		
-$(function() {		
-	timer();		
-})
+var $slides = $('.slide'),
+		$firstSlide = $slides[0],
+		$lastSlide = $slides[$slides.length - 1],
+		$dots = $('.dot'),
+		$firstDot = $dots[0],
+		$lastDot = $dots[$dots.length - 1],
+		timer;
 
-$('.arrow').click(function() {
-	clearInterval(timer());
-
-	if ($(this).attr('id') == 'arrow-prev') {
-		prevSlide();
-	} else {
-		nextSlide();
-	}
-})
-
-function prevSlide() {
-	changeSlide($firstSlide, $lastSlide, $lastDot, -1);
-}
-
-function nextSlide() {
-	changeSlide($lastSlide, $firstSlide, $firstDot, 1);	
+function setTimer() {
+	clearInterval(timer);
+	timer = setInterval(function() { nextSlide() }, 5000);
 }
 
 function changeSlide(moveFrom, moveTo, dot, direction) {
 	var $activeSlide = $('.active-slide')[0],
-		$activeDot = $('.active-dot')[0];
+			$activeDot = $('.active-dot')[0];
 
 	$activeSlide.className = 'slide';
 	$activeDot.className = 'dot';
@@ -42,14 +25,37 @@ function changeSlide(moveFrom, moveTo, dot, direction) {
 		$slides[$slides.index($activeSlide) + direction].className = 'active-slide slide';
 		$dots[$dots.index($activeDot) + direction].className = 'active-dot dot';
 	}
+
+	setTimer();
 }
+
+function nextSlide() {
+	changeSlide($lastSlide, $firstSlide, $firstDot, 1);
+}
+
+function prevSlide() {
+	changeSlide($firstSlide, $lastSlide, $lastDot, -1);
+}
+
+$('.arrow').click(function() {
+	if ($(this).attr('id') == 'arrow-next')
+		nextSlide();
+	else
+		prevSlide();
+});
 
 $dots.click(function() {
 	var idx = $dots.index($(this));
-	
+
 	$('.active-dot')[0].className = 'dot';
 	$dots[idx].className = 'active-dot dot';
 
 	$('.active-slide')[0].className = 'slide';
 	$slides[idx].className = 'active-slide slide';
-})
+
+	setTimer();
+});
+
+$(function() {
+	setTimer();
+});
